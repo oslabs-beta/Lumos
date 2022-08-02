49c0a7bc837d1577659f159ecc0cdefc0b58f98e
Addy Chandrashekar <addy@Addys-MacBook-Pro-2.local>
2022-08-02 11:32:44 -0400

    Working app

---
+++ client/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button, Card } from "@mui/material";
+
+class App extends React.Component {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1>Hello {name}</h1>
+        <Button variant="contained">this is a material UI button</Button>
+      </>
+    );
+  }
+}
+
+export default App;
