34c494300d0fa3bf3c69111b0a408ef0ddbaf4f1
InternalShadow <76181682+InternalShadow@users.noreply.github.com>
2022-08-10 21:54:10 -0400

    carmen said to stop

--- server/aws/getLambdaFuncs.js
+++ server/aws/getLambdaFuncs.js
@@ -1,7 +1,6 @@
 const {
   LambdaClient,
   ListFunctionsCommand,
-  GetFunctionCommand,
 } = require('@aws-sdk/client-lambda');
 
 
@@ -11,26 +10,33 @@ const getLambdaFuncs = async () => {
     const client = new LambdaClient({ region: 'us-east-1' });
     const command = new ListFunctionsCommand({ FunctionVersion: 'ALL' });
     const response = await client.send(command);
-    console.log(response.Functions);
     // This returns FunctionArn and FunctionName
     const funcNames = response.Functions.map((func) => func.FunctionName)
-    console.log(funcNames)
     return funcNames;
   } catch (err) {
     return err;
   }
 };
 
-const getFunc = async () => {
-  try {
-    const client = new LambdaClient({ region: 'us-east-1' });
-    const command = new GetFunctionCommand({ FunctionName: 'arn:aws:lambda:us-east-1:528832648632:function:add:$LATEST' });
-    const response = await client.send(command)
-    console.log(response)
-  } catch (err) {
-    return err
-  }
-}
+// const allLambdaFuncs = [
+//   {
+//   funcName: "", // label
+//   totalInvocations: this.funcValues.reduce((a, b) => a + b, 0), // total sum
+//   totalErrors: 0,
+//   timestamps: [],
+//   funcValues: []
+//   }
+// ]
 
-getLambdaFuncs()
+// const getFunc = async () => {
+//   try {
+//     const client = new LambdaClient({ region: 'us-east-1' });
+//     const command = new GetFunctionCommand({ FunctionName: 'arn:aws:lambda:us-east-1:528832648632:function:add:$LATEST' });
+//     const response = await client.send(command)
+//     console.log(response)
+//   } catch (err) {
+//     return err
+//   }
+// }
+
 module.exports = getLambdaFuncs;
