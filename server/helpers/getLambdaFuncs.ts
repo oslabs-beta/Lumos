8806380356683a5300a34106d80b92d97f490487
carmen <72165737+BadWithNames@users.noreply.github.com>
2023-05-26 19:27:25 -0400

    create getLambdaFuncs helper

---
+++ server/helpers/getLambdaFuncs.ts
@@ -0,0 +1,19 @@
+import {
+  LambdaClient,
+  ListFunctionsCommand,
+  ListFunctionsResponse,
+} from '@aws-sdk/client-lambda';
+
+// This functions returns an array Lambda func names
+
+export default async function getLambdaFuncs(): Promise<string[]> {
+  try {
+    const client = new LambdaClient({ region: 'us-east-1' });
+    const command = new ListFunctionsCommand({ FunctionVersion: 'ALL' });
+    const response: ListFunctionsResponse = await client.send(command); // This returns FunctionArn and FunctionName
+    return response.Functions?.map((f) => f.FunctionName);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
