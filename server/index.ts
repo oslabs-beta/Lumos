b31c3398b8fee181c8d10c16ead508052761d1eb
carmen <72165737+BadWithNames@users.noreply.github.com>
2023-05-26 19:26:18 -0400

    mount authRouter, protect metric routes, passport

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,32 @@
 import express, { Express } from 'express';
+import session from 'express-session';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import metricRouter from './routes/metricRouter';
+import authRouter from './routes/authRouter';
+import passport from './config/passport';
 
 dotenv.config();
-
 const app: Express = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRouter);
+app.use('/metric', passport.authenticate('local'), metricRouter);
 
 app.listen(process.env.PORT, () =>
   console.log(`⚡️ Server running on http://localhost:${process.env.PORT} ⚡️`),
